749dcfd3517ef63bb2e99e7cf57a76fdaae9e133
Edward Chau <edwardchau@berkeley.edu>
2020-06-04 22:12:37 -0700

    new

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function init() {
     modal7()
     modal8()
     modal9()
+    modal10()
 }
 
 function modal1() {
@@ -214,6 +215,29 @@ function modal9() {
 
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close9")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() { 
+        modal.style.display = "none";
+    }
+}
+
+function modal10() {
+    // Get the modal
+    var modal = document.getElementById("myModal10");
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.getElementById("myImg10");
+    var modalImg = document.getElementById("img10");
+    var captionText = document.getElementById("caption10");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close10")[0];
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() { 
